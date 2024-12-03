fe0512650e03725afbf4f080f1b0cabcc635430c
B-iNi <bini392573@gmail.com>
2024-12-04 00:36:40 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
-//코드잇 자바스크립트 공부
-//과제3. 파라미터 응용 함수 호출함
-function teraToGiga(a){
-    console.log(a + 'TB는');
-    console.log(a * 1024 +'GB 입니다.');
-    
-  }
-  function teraToMega(b){
-    console.log(b + 'TB는');
-    console.log(b * 1024 * 1024 +'MB 입니다.');
-  }
-  teraToGiga(2);
-  teraToMega(2);
+/***************************************************************//코드잇 자바스크립트 공부//***************************************** */
 
-  //과제4.   same
-  function bmiCalculator(name,weight,height){
+
+                                                           //과제5. 파라미터 응용 함수 호출 계산
+ function interestCalculator(amount, saveTerm, interestRate){
+  let money = amount * saveTerm * interestRate / 100;
+  let total = amount + money
+ 
+  console.log('맡긴 금액은 ' + amount + '원 입니다.');
+  console.log('이자는 ' + money +'원 입니다.');
+  console.log('최종 받을 금액은 ' + total + '원 입니다.');
+}
+interestCalculator(3650000, 1, 4);
+
+                                                           //과제4. 파라미터 응용 함수 호출 계산
+function bmiCalculator(name,weight,height){
     console.log(name + `님의 체질량지수는 `+ weight/(height*height/10000)+`입니다.`);
-  }
+}
   
   // 테스트 코드
   bmiCalculator('홀쭉이', 43.52, 160);
   bmiCalculator('코린이', 61.25, 175);
   bmiCalculator('통통이', 77.76, 180);
+
+
+                                                           //과제3. 파라미터 응용 함수 호출함
+  function teraToGiga(a){
+    console.log(a + 'TB는');
+    console.log(a * 1024 +'GB 입니다.');
+    
+}
+  function teraToMega(b){
+    console.log(b + 'TB는');
+    console.log(b * 1024 * 1024 +'MB 입니다.');
+}
+  teraToGiga(2);
+  teraToMega(2);
+
+                                                           //과제2. 함수 이용 출력하기
+  function printChorus(){
+    console.log(`무구궁화 삼천리 화려 강산\n대한 사람 대한으로 길이 보전하세`);
+}
+                                                                                                //function 함수를 호출하고 정의한다
+                                                                                                //JS 에서는 'let'
+
+console.log('1절');
+console.log('동해 물과 백두산이 마르고 닳도록');
+console.log('하느님이 보우하사 우리나라 만세');
+ 
+console.log('2절');
+console.log('남산 위에 저 소나무 철갑을 두른 듯');
+console.log('바람서리 불변함은 우리 기상일세');
+ 
+console.log('3절');
+console.log('가을 하늘 공활한데 높고 구름 없이');
+console.log('밝은 달은 우리 가슴 일편단심일세');
+ 
+console.log('4절');
+console.log('이 기상과 이 맘으로 충성을 다하여');
+console.log('괴로우나 즐거우나 나라 사랑하세');
+
+
+                                                           //과제1. 15라는 숫자 3번 입력해 보기
+
+console.log(10+5);
+console.log(10*3/2);
+console.log(45/3);
+
+
